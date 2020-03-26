33e20e1e5c2203ba9b38408fbb70243454e3d6e9
Melonai <einebeere@gmail.com>
2020-03-26 23:01:55 +0100

    port to discord.js, cleanup, modular commands started

--- bot.js
+++ bot.js
@@ -1,87 +1,96 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
+const fs = require('fs');
+const Discord = require('discord.js');
+const winston = require('winston');
+
+const auth = require('./auth.json');
+const config = require('./config.json');
+
+const logger = winston.createLogger({
+    level: 'debug',
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        winston.format.printf(info => `[C] ${info.timestamp} ${info.level}: ${info.message}`)
+    ),
+    transports: [
+        new winston.transports.Console()
+    ]
 });
 
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+
+bot.once('ready', () => {
+    logger.info('Ready!');
 });
 
-function commands(user, userID, channelID, message, evt) {
+bot.login(auth.token).then( () => {
+    logger.info('Logged in as: ');
+    logger.info(bot.user.username + ' - (' + bot.user.id + ')');
+});
 
-    logger.info('Command taken!')
+bot.on('message', message => {
+    config.prefixes.forEach((prefix) => {
+        if (message.content.startsWith(prefix)) {
+            logger.info(`Message recognized as a "${prefix}" command!`);
+            command(message, message.content.substring(prefix.length));
+        }
+    });
+});
 
-    var args = message.split(' ');
-    var cmd = args[0].toLowerCase();
-    args = args.splice(1);
+function commands_old(message, content) {
 
-    logger.info(args)
-    logger.info(cmd)
+    logger.info('Command taken!');
 
-    var questionwords = ["will", "can", "is", "does", "are", "was", "do", "don't", "doesn't", "am"];
-    var questionanwsers = ["Absolutely", "Definitely!", "Probably!", "That'd be right my dude!", "Fuck yeah!", "Nope.", "WTF no!", "Get away from me!!", "Yeaa... No.", "Let's just say it's... up for debate.."];
+    const args = content.split(' ');
+    const cmd = args.shift().toLowerCase();
+
+    const questionwords = ["will", "can", "is", "does", "are", "was", "do", "don't", "doesn't", "am", "did"];
+    const questionanwsers = ["Absolutely", "Definitely!", "Probably!", "That'd be right my dude!", "Fuck yeah!", "Nope.", "WTF no!", "Get away from me!!", "Yeaa... No.", "Let's just say it's... up for debate.."];
 
 
     if (questionwords.indexOf(cmd) > -1) {
-        logger.info('Command is a question!')
-        bot.sendMessage({to: channelID, message: questionanwsers[Math.floor(Math.random() * questionanwsers.length)]});
+        logger.info('Command is a question!');
+        message.channel.send(questionanwsers[Math.floor(Math.random() * questionanwsers.length)]);
     } else {
         logger.info('Command is not a question.')
     }
 
-    if (cmd == "die") {
-        bot.sendMessage({to: channelID, message: "Ok.. :C"});
-        bot.disconnect();
+    if (cmd === "die") {
+        message.channel.send("No.");
     } else {
-        logger.info('Command is not "kill".')
+        logger.info('Command is not "kill".');
     }
 
-    if (cmd == "say") {
-        bot.deleteMessage({channelID: channelID, messageID: evt.d.id})
-        bot.sendMessage({to: channelID, message: args.join(" ")});
+    if (cmd === "say") {
+        message.delete();
+        message.channel.send(args.join(" "));
     } else {
-        logger.info('Command is not "say".')
+        logger.info('Command is not "say".');
     }
 
-    if (cmd == "hello!") {
-        bot.sendMessage({to: channelID, message: "Hiiii! ^^"});
+    if (cmd === "hello!") {
+        message.channel.send("Hiiii! ^^");
     } else {
-        logger.info('Command is not "hello!".')
+        logger.info('Command is not "hello!".');
     }
 
-    if (cmd == "generate") {
-        var high = parseInt(args[5]);
-        var low = parseInt(args[3]);
-        bot.sendMessage({to: channelID, message: Math.round(Math.random() * (high - low) + low).toString()});
+    if (cmd === "generate") {
+        const high = parseInt(args[5]);
+        const low = parseInt(args[3]);
+        message.channel.send(Math.round(Math.random() * (high - low) + low).toString());
     }
 }
 
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
+function command(message, content) {
+    const args = content.split(' ');
+    const cmd = args.shift().toLowerCase();
 
-bot.on('message', function (user, userID, channelID, message, evt) {
 
-    logger.info('Message recieved!')
+}
 
-    if (message.substring(0, 1) == '+') {
-        logger.info('Message Recognized as a "+" command!')
-
-        commands(user, userID, channelID, message.substring(1), evt);
-
-     } else if (message.substring(0, 5).toString().toLowerCase() == 'carl,') {
-
-        logger.info('Message Recognized as a "Carl," command!')
-
-        commands(user, userID, channelID, message.substring(6), evt);
-     } 
-});
+bot.addCommand = (path) => {
+    //bot.commands.set(path.)
+};
---
+++ commands/ping.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'Ping',
+    description: 'Pong.',
+    handles: ['ping'],
+    execute: main
+};
+
+function main(message, args) {
+    message.channel.send('Ping.')
+}
