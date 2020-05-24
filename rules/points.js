d4d4cfa5c308a886adf784af239de9d419fdacf3
Melonai <einebeere@gmail.com>
2020-05-24 03:13:08 +0200

    points timeout added

--- rules/points.js
+++ rules/points.js
@@ -8,11 +8,15 @@ module.exports = new Rule({
 });
 
 async function main(message) {
-    if (message.client.database.connectionExists()) {
-        await message.client.database.addPoints(message.author, 1, rowCount => {
-            if (rowCount === 0) {
-                message.client.database.addUser(message.author);
-            }
-        });
+    const currentTime = new Date().getTime();
+    if (!message.author.lastMessageTimestamp || message.author.lastMessageTimestamp + 60000 < currentTime) {
+        if (message.client.database.connectionExists()) {
+            await message.client.database.addPoints(message.author, 1, rowCount => {
+                if (rowCount === 0) {
+                    message.client.database.addUser(message.author);
+                }
+                message.author.lastMessageTimestamp = currentTime;
+            });
+        }
     }
 }
