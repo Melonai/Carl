e7e4e3bc83657b40f197b965092cf1c34c8e350c
Melonai <einebeere@gmail.com>
2020-07-28 00:10:36 +0200

    Channel lock message changed

--- commands/channel-lock.js
+++ commands/channel-lock.js
@@ -11,7 +11,7 @@ async function main(command, message) {
     const lockData = message.guild.data.locks;
     const channelID = message.channel.id;
     if (!lockData.includes(message.channel.id)) {
-        await command.client.send("The channel has now been unlocked", message.channel);
+        await command.client.send("This channel has now been locked.", message.channel);
         lockData.push(channelID);
     }
 }
